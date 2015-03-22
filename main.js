b59531241ffd82bf1c4cebaef0b2263929d41f92
Piotr Halicki <Piotr_Halicki@wp.pl>
2015-03-22 21:18:30 +0100

    Dodany fadeOut.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$('.blue').animate({	opacity: 10, height: '120px', width: '120px', left: '378px' }, 
+$('.blue').animate({ opacity: 10, height: '120px', width: '120px', left: '378px' }, 
 1500, function(){
 	
 $('.green').animate({ opacity: 10, height: '120px', width: '120px', left: '10px'	}, 
@@ -13,10 +13,16 @@ $('.violet').animate({ opacity: 10,	height: '120px', width: '120px', left: '60px
 $('.red').animate({	opacity: 10, height: '120px', width: '120px', right: '60px'	},
 1000, function(){
 	
-$('.red').animate({ opacity: 0	},
-1000, function(){
+$('.red').fadeOut(6000);	
+$('.violet').fadeOut(4000);	
+$('.greenR').fadeOut(3000);	
+$('.green').fadeOut(2000);	
+$('.blue').fadeOut(1000);	
+	
+//$('.red').animate({ opacity: 0	},
+//1000, function(){
 
-});
+// });
 });
 });
 });
