5f013fee102318eb48bf5cea6124bb657352acb8
Piotr Halicki <Piotr_Halicki@wp.pl>
2015-03-22 02:42:03 +0100

    jQuery - Animacja :)

---
+++ main.js
@@ -0,0 +1,24 @@
+$('.blue').animate({	opacity: 10, height: '120px', width: '120px', left: '378px' }, 
+1500, function(){
+	
+$('.green').animate({ opacity: 10, height: '120px', width: '120px', left: '10px'	}, 
+1000, function(){	
+		
+$('.greenR').animate({ opacity: 10, height: '120px', width: '120px', left: '60px'	}, 
+1000, function(){	
+			
+$('.violet').animate({ opacity: 10,	height: '120px', width: '120px', left: '60px' }, 
+1000, function(){	
+	
+$('.red').animate({	opacity: 10, height: '120px', width: '120px', right: '60px'	},
+1000, function(){
+	
+$('.red').animate({ opacity: 0	},
+1000, function(){
+
+});
+});
+});
+});
+});
+});
